f12a73bdf0cc7c4184b2d7c167de68f0c5bfd0f0
tranki1 <kimhieutran100@gmail.com>
2018-05-14 21:16:35 +0300

    add search function to the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, Route} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Bookshelf, {getShelfTypes,getShelfTypeName} from './Component/Bookshelf'
+import Search from './Component/Search'
 import './App.css'
 
 
@@ -38,28 +39,10 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route path="/search" render={({history})=> (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link
-                to='/'
-                className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search
+            searchResult={this.state.books}
+            onUpdateBook={this.updateBook}
+          />
         )}/>
 
         <Route exact path='/' render={()=>(
---
+++ src/Component/Search.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import Bookshelf from '../Component/Bookshelf';
+import * as BooksAPI from '../BooksAPI';
+
+
+class Search extends Component{
+  state ={
+    books:[],
+    query:''
+  }
+
+  updateQuery = (query) => {
+    if (!query) {
+      this.setState({query:'',books:[]})
+    }
+    this.setState({query : query.trim()})
+    /*
+      NOTES: The search from BooksAPI is limited to a particular set of search terms.
+      You can find these search terms here:
+      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+      you don't find a specific author or title. Every search is limited by search terms.
+    */
+    BooksAPI.search(query).then((books)=>{
+      if ('error' in books){
+        books=[]
+      }
+      else {
+        /*
++				 * Since the search API didn't return the shelf property for a book this will compare with
++				 * memory mapped books from the shelves to include the correct shelf attribute.
++				 */
+				books.map(book => (this.props.searchResult.filter((b) => b.id === book.id).map(b => book.shelf = b.shelf)));
+      }
+      this.setState({books:books});
+    })
+  }
+  render(){
+    const onUpdateBook = this.props.onUpdateBook;
+    return(
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link
+            to='/'
+            className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              className='search-books'
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(e)=>this.updateQuery(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <Bookshelf
+            books={this.state.books}
+            onUpdateBook={onUpdateBook}
+          />
+        </div>
+      </div>
+
+    )
+  }
+}
+
+Search.propTypes = {
+  searchResult: PropTypes.array.isRequired,
+  onUpdateBook:PropTypes.func.isRequired
+}
+
+export default Search;
