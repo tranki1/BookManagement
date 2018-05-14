ed59cb6d2329d2a3d2615f238e2a7d0926b8999b
tranki1 <kimhieutran100@gmail.com>
2018-05-14 23:04:28 +0300

    book transition + download Material IU theme through npm

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class BooksApp extends React.Component {
         <Route exact path='/' render={()=>(
           <div className="list-books">
             <div className="list-books-title">
-              <h1>MyReads</h1>
+              <h1>Book Management Tool</h1>
             </div>
             <div className="list-books-content">
               <div>
--- src/Component/Bookshelf.js
+++ src/Component/Bookshelf.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'react-loader-advanced';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 const SHELF_TYPE_IDS = [
   'currentlyReading',
@@ -31,33 +32,39 @@ class Bookshelf extends Component{
   render(){
     const {books, onUpdateBook} = this.props;
     return(
-      <ol className="books-grid">
-        {books.map((book)=>(
-          <li key={book.id}>
-            <Loader
-              show={('loading' in book)? book.loading : false}
-              message={<span><img src="three-dots.svg" width="50" alt=""/><div>Loading</div></span>}>
+      <ol>
+        <CSSTransitionGroup
+  					transitionName="book-transition"
+  					className="books-grid"
+  					transitionEnterTimeout={500}
+  					transitionLeaveTimeout={300}>
+          {books.map((book)=>(
+            <li key={book.id}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                  <Loader
+                    show={('loading' in book)? book.loading : false}
+                    message={<span><img src="../public/three-dots.svg" width="50" alt=""/><div>Loading</div></span>}>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                  </Loader>
                   <div className="book-shelf-changer">
-                    <select
-                      onChange ={(e)=> onUpdateBook(book,e.target.value)}
-                      value={book.shelf}>
-                      <option disabled>Move to...</option>
-                      {getShelfTypes().map((shelf)=>(
-                        <option key={shelf} value={shelf}>{getShelfTypeName(shelf)}</option>
-                      ))}
-                      <option value="none">None</option>
-                    </select>
+                      <select
+                        onChange ={(e)=> onUpdateBook(book,e.target.value)}
+                        value={book.shelf}>
+                        <option disabled>Move to...</option>
+                        {getShelfTypes().map((shelf)=>(
+                          <option key={shelf} value={shelf}>{getShelfTypeName(shelf)}</option>
+                        ))}
+                        <option value="none">None</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{('authors' in book) ? book.authors.join(', ') : ''}</div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{('authors' in book) ? book.authors.join(', ') : ''}</div>
               </div>
-            </Loader>
-          </li>
-        ))}
+            </li>
+          ))}
+        </CSSTransitionGroup>
       </ol>
     )
   }
